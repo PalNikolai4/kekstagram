b3f2fd5ade8f613bf08a33c6fda42fcdf678fb6b
Pavel <paha-87@mail.ru>
2023-05-15 23:45:03 +0300

    add Fn closeFullPhoto

--- js/modules/full-picture.js
+++ js/modules/full-picture.js
@@ -1,6 +1,13 @@
 const bigPictureContainer = document.querySelector('.big-picture');
 const bigPictureSocial = bigPictureContainer.querySelector('.big-picture__social');
 
+const closeFullPhoto = () => {
+  const closeButton = bigPictureContainer.querySelector('.big-picture__cancel');
+  closeButton.addEventListener('click', () => {
+    bigPictureContainer.classList.add('hidden');
+  })
+}
+
 const showFullPhoto = (item) => {
   item.addEventListener('click', function (evt) {
     evt.preventDefault();
@@ -8,6 +15,7 @@ const showFullPhoto = (item) => {
     bigPictureContainer.querySelector('.big-picture__img').querySelector('img').src = item.querySelector('.picture__img').src;
     bigPictureSocial.querySelector('.likes-count').textContent = item.querySelector('.picture__likes').textContent;
     bigPictureSocial.querySelector('.comments-count').textContent = item.querySelector('.picture__comments').textContent;
+    closeFullPhoto();
   })
 }
 
