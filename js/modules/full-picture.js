7485b5b92e43e6cc2f7bba663e137d1231bd8bfe
Pavel <paha-87@mail.ru>
2023-05-19 21:39:04 +0300

    added Fn toggleModalOpen

--- js/modules/full-picture.js
+++ js/modules/full-picture.js
@@ -1,13 +1,20 @@
-import {createElement} from './utill.js';
-
+import { createElement } from './utill.js';
 const bigPictureContainer = document.querySelector('.big-picture');
+
+/**
+ * Fn toggle body class 'modal-open'
+ */
+const toggleModalOpen = () => {
+  const isHidden = bigPictureContainer.classList.contains('hidden');
+  isHidden ? document.body.classList.remove('modal-open') : document.body.classList.add('modal-open');
+}
 
 /**
  * Fn takes the comment data as an object and creates a comment
  * @param {object} param0
  * @returns
  */
-const createComment = ({avatar, name, message}) => {
+const createComment = ({ avatar, name, message }) => {
   const userComment = createElement('li', 'social__comment');
 
   const userCommentPicture = createElement('img', 'social__picture');
@@ -28,12 +35,14 @@ const closeFullPhoto = () => {
   const closeButton = bigPictureContainer.querySelector('.big-picture__cancel');
   closeButton.addEventListener('click', () => {
     bigPictureContainer.classList.add('hidden');
+    toggleModalOpen();
   });
 
   document.addEventListener('keydown', (evt) => {
     evt.preventDefault();
     if (evt.keyCode === 27) {
       bigPictureContainer.classList.add('hidden');
+      toggleModalOpen();
     }
   });
 };
@@ -41,17 +50,20 @@ const closeFullPhoto = () => {
 const showFullPhoto = ({ url, description, likes, comments }) => {
   bigPictureContainer.classList.remove('hidden');
   bigPictureContainer.querySelector('.big-picture__img').querySelector('img').src = url;
-  // console.log(thumbnail);
   bigPictureContainer.querySelector('.likes-count').textContent = likes;
   bigPictureContainer.querySelector('.comments-count').textContent = comments.length;
   bigPictureContainer.querySelector('.social__caption').textContent = description;
 
+  const commentsFragment = document.createDocumentFragment();
+  const socialCommentsList = bigPictureContainer.querySelector('.social__comments');
+  socialCommentsList.innerHTML = '';
   comments.forEach((comment) => {
     const userComment = createComment(comment);
-    // console.log(userComment)
+    commentsFragment.append(userComment);
   });
-
+  socialCommentsList.append(commentsFragment);
   closeFullPhoto();
+  toggleModalOpen();
 };
 
 export { showFullPhoto };
--- js/modules/gallery.js
+++ js/modules/gallery.js
@@ -5,14 +5,13 @@ import { showFullPhoto } from './full-picture.js';
 
 const picturesContainer = document.querySelector('.pictures');
 const arrayDescriptionsPhotos = getArrayGivenLength(getDescriptionPhoto, 1, 25, 5);
-// console.log(arrayDescriptionsPhotos)
+
 const thumbnailsInPage = drawArrayDescriptionsPhotos(arrayDescriptionsPhotos);
 const thumbnails = picturesContainer.querySelectorAll('a.picture');
 
 for (let i = 0; i < thumbnails.length; i++) {
   thumbnails[i].addEventListener('click', (evt) => {
     evt.preventDefault();
-    showFullPhoto(arrayDescriptionsPhotos[i]); // сюда (и в обьявлении) добавить второй параметр - это будет arrayDescriptionsPhotos. В функции
-    // createComment прописать адреса из второго параметра функции showFullPhoto
+    showFullPhoto(arrayDescriptionsPhotos[i]);
   });
 }
